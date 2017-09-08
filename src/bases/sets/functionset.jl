0887c253973adb1625ee602aafd8d9513c0fb0b9
vincentcp <vincent.coppe@gmail.com>
2017-09-08 09:25:59 +0200

    add option to not extend with a value

--- src/bases/sets/functionset.jl
+++ src/bases/sets/functionset.jl
@@ -381,9 +381,9 @@ the support. This value can be changed with an optional extra argument.
 
 After the checks, this routine calls eval_element on the concrete set.
 """
-function eval_set_element(set::FunctionSet, idx, x, outside_value = zero(eltype(set)))
+function eval_set_element(set::FunctionSet, idx, x, outside_value = zero(eltype(set)); extend=false)
     checkbounds(set, idx)
-    in_support(set, idx, x) ? eval_element(set, idx, x) : outside_value
+    extend || in_support(set, idx, x) ? eval_element(set, idx, x) : outside_value
 end
 
 # We use a special routine for evaluation on a grid, since we can hoist the boundscheck.
@@ -406,14 +406,14 @@ end
 """
 Evaluate an expansion given by the set of coefficients `coefficients` in the point x.
 """
-function eval_expansion(set::FunctionSet, coefficients, x)
+function eval_expansion(set::FunctionSet, coefficients, x; options...)
     T = promote_type(eltype(coefficients), eltype(set))
     z = zero(T)
 
     # It is safer below to use eval_set_element than eval_element, because of
     # the check on the support. We elide the boundscheck with @inbounds (perhaps).
     @inbounds for idx in eachindex(set)
-        z = z + coefficients[idx] * eval_set_element(set, idx, x)
+        z = z + coefficients[idx] * eval_set_element(set, idx, x; options...)
     end
     z
 end
