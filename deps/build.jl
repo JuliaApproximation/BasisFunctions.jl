ba2837fc3f8999b0d67863378c7c110d32d0ae50
vincentcp <vincent.coppe@gmail.com>
2018-07-16 11:58:29 +0200

    fix LinearAlgebra updates

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,4 @@
 Pkg.clone("https://github.com/vincentcp/CardinalBSplines.git")
 Pkg.clone("https://github.com/vincentcp/WaveletsCopy.jl.git")
-Pkg.clone("https://github.com/andreasnoack/LinearAlgebra.jl")
+Pkg.clone("https://github.com/andreasnoack/GenericLinearAlgebra.jl")
+Pkg.checkout("GenericLinearAlgebra","2915f28247cbedf3abb204afa33811865a785fb9")
