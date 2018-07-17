3b29eacf34439a254468cb4ed397cc8d8eb6f751
vincentcp <vincent.coppe@gmail.com>
2018-07-17 23:00:59 +0200

    change travis

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,3 @@
-Pkg.clone("https://github.com/vincentcp/WaveletsCopy.jl.git")
+Pkg.clone("https://github.com/vincentcp/WaveletsCopy.jl.git","development")
 Pkg.clone("https://github.com/vincentcp/LinearAlgebra.jl")
 Pkg.clone("https://github.com/daanhb/Domains.jl")
