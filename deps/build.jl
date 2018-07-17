b02fc22e204f12dd55c863598a2beabf09c551cb
vincentcp <vincent.coppe@gmail.com>
2018-07-17 11:39:09 +0200

    Change LinearAlgebra repo

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,3 @@
 Pkg.clone("https://github.com/vincentcp/CardinalBSplines.git")
 Pkg.clone("https://github.com/vincentcp/WaveletsCopy.jl.git")
-Pkg.clone("https://github.com/andreasnoack/GenericLinearAlgebra.jl")
-Pkg.checkout("GenericLinearAlgebra","2915f28247cbedf3abb204afa33811865a785fb9")
+Pkg.clone("https://github.com/vincentcp/LinearAlgebra.jl")
