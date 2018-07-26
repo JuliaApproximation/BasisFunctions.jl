35f922502fbe406373a3178b775b99eb9b0cc608
vincentcp <vincent.coppe@gmail.com>
2018-07-26 20:59:51 +0200

    Change domains remote location

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,4 @@
 if VERSION < v"0.7-"
     Pkg.clone("https://github.com/vincentcp/LinearAlgebra.jl.git")
-    Pkg.clone("https://github.com/daanhb/Domains.jl")
+    Pkg.clone("https://github.com/JuliaApproximation/Domains.jl.git")
 end
