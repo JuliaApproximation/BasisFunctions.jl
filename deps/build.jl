bba455296913eaf90d9d8a39da0c3626f93ff555
vincentcp <vincent.coppe@gmail.com>
2018-10-16 14:25:04 +0200

    use right Domains branch

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,10 @@
 if VERSION < v"0.7-"
     Pkg.clone("https://github.com/vincentcp/LinearAlgebra.jl.git")
-    Pkg.clone("https://github.com/JuliaApproximation/Domains.jl.git")
-    Pkg.checkout("Domains", "julia-0.6-compatible")
+    try
+        Pkg.clone("https://github.com/JuliaApproximation/Domains.jl.git")
+    catch y
+        nothing
+    finally
+        Pkg.checkout("Domains", "julia-0.6-compatible")
+    end
 end
