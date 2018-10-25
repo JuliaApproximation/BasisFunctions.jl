1c86721741e0d3d43631c4f8cc3cb67dedb48c61
vincentcp <vincent.coppe@gmail.com>
2018-10-25 09:52:19 +0200

    try to install GenericLinearAlgebra

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,4 @@
+
 if VERSION < v"0.7-"
     Pkg.clone("https://github.com/vincentcp/LinearAlgebra.jl.git")
     try
@@ -6,5 +7,10 @@ if VERSION < v"0.7-"
         nothing
     finally
         Pkg.checkout("Domains", "julia-0.6-compatible")
+    end
+else
+    using Pkg
+    if !in("GenericLinearAlgebra", keys(Pkg.installed()))
+        Pkg.add(PackageSpec(url="http://github.com/andreasnoack/GenericLinearAlgebra.jl"))
     end
 end
