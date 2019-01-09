6c968f001a0fe13fc46fe8e1ac1ad07b7c6b52df
vincentcp <vincent.coppe@gmail.com>
2019-01-09 12:00:24 +0100

    Reorder Project.toml

--- deps/build.jl
+++ deps/build.jl
@@ -1,16 +1,5 @@
 
-if VERSION < v"0.7-"
-    Pkg.clone("https://github.com/vincentcp/LinearAlgebra.jl.git")
-    try
-        Pkg.clone("https://github.com/JuliaApproximation/DomainSets.jl.git")
-    catch y
-        nothing
-    finally
-        Pkg.checkout("DomainSets", "julia-0.6-compatible")
-    end
-else
-    using Pkg
-    if !in("GenericLinearAlgebra", keys(Pkg.installed()))
-        Pkg.add(PackageSpec(url="http://github.com/andreasnoack/GenericLinearAlgebra.jl"))
-    end
+using Pkg
+if !in("GenericLinearAlgebra", keys(Pkg.installed()))
+    Pkg.add(PackageSpec(url="http://github.com/andreasnoack/GenericLinearAlgebra.jl"))
 end
