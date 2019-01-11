67158bfad17c03c97a88340cd157387572afa918
vincentcp <vincent.coppe@gmail.com>
2019-01-11 10:17:22 +0100

    remove Extend and Restrict

--- src/generic/extension.jl
+++ src/generic/extension.jl
@@ -2,59 +2,19 @@
 ####################################
 # Generic extension and restriction
 ####################################
-
-# A dictionary can often be extended or restricted to a larger or smaller one.
-# Extension and restriction operators always have a source and destination set.
-# For introspection, you can ask a set what a suitable extensize size would be,
-# and use a resized set as the destination of the operator:
-#
-#   larger_size = extension_size(some_set)
-#   larger_set = resize(some_set, larger_size)
-#   E = extension_operator(some_set, larger_set)
-#
-# and similarly for restriction. This is the default if no destination set is
-# supplied to extension_operator.
-#
-# If a set does not need to store data for its extension operator, it can simply
-# choose to implement the action of the Extension operator, e.g.:
-#
-# apply!(op::Extension, s1::SomeSet, s2::SomeSet, coef_dest, coef_src) = ...
-
-struct Extension{SRC <: Dictionary,DEST <: Dictionary,ELT} <: DictionaryOperator{ELT}
-    src     ::  SRC
-    dest    ::  DEST
-end
-
-function Extension(src::Dictionary, dest::Dictionary)
-    @warn "The Extension operator will be removed in the future, please implement a specific operator."
-    Extension{typeof(src),typeof(dest),op_eltype(src,dest)}(src, dest)
-end
-
-function Extension(::Type{ELT}, src::Dictionary, dest::Dictionary) where {ELT}
-    S, D, A = op_eltypes(src, dest, ELT)
-    new_src = promote_coefficienttype(src,S)
-    new_dest = promote_coefficienttype(dest,D)
-    Extension(new_src, new_dest)
-end
-
-similar_operator(op::Extension, ::Type{S}, src::Dictionary, dest::Dictionary) where {S} =
-    Extension(S, src, dest)
-
-"""
+ """
 An extension operator is an operator that can be used to extend a representation in a set s1 to a
-representation in a larger set s2. The default extension operator is of type Extension with s1 and
-s2 as source and destination.
+representation in a larger set s2.
 """
-function extension_operator(s1::Dictionary, s2::Dictionary; options...)
-    @assert hasextension(s1)
-    default_extension_operator(s1, s2; options...)
-end
+function extension_operator(s1::Dictionary, s2::Dictionary; options...) end
 
-# default_extension_operator(s1::Dictionary, s2::Dictionary; options...) =
-#     IndexExtensionOperator(s1, s2, 1:length(s1))
-default_extension_operator(s1::Dictionary, s2::Dictionary; options...) =
-    Extension(s1, s2)
 
+"""
+A restriction operator does the opposite of what the extension operator does: it restricts
+a representation in a set s1 to a representation in a smaller set s2. Loss of accuracy may result
+from the restriction.
+"""
+function restriction_operator(s1::Dictionary, s2::Dictionary; options...) end
 
 """
 Return a suitable length to extend to, for example one such that the corresponding grids are nested
@@ -68,49 +28,10 @@ extension_operator(s1::Dictionary; options...) =
     extension_operator(s1, extend(s1); options...)
 
 
-struct Restriction{SRC <: Dictionary,DEST <: Dictionary,ELT} <: DictionaryOperator{ELT}
-    src     ::  SRC
-    dest    ::  DEST
-end
-
-function Restriction(src::Dictionary, dest::Dictionary)
-    @warn "The Restriction operator will be removed in the future, please implement a specific operator."
-    Restriction{typeof(src),typeof(dest),op_eltype(src,dest)}(src, dest)
-end
-
-function Restriction(::Type{ELT}, src::Dictionary, dest::Dictionary) where {ELT}
-    S, D, A = op_eltypes(src, dest, ELT)
-    new_src = promote_coefficienttype(src,S)
-    new_dest = promote_coefficienttype(dest,D)
-    Restriction(new_src, new_dest)
-end
-
-similar_operator(op::Restriction, ::Type{S}, src::Dictionary, dest::Dictionary) where {S} =
-    Restriction(S, src, dest)
-
-
-"""
-A restriction operator does the opposite of what the extension operator does: it restricts
-a representation in a set s1 to a representation in a smaller set s2. Loss of accuracy may result
-from the restriction. The default restriction_operator is of type Restriction with sets s1 and
-s2 as source and destination.
-"""
-function restriction_operator(s1::Dictionary, s2::Dictionary; options...)
-    @assert hasextension(s2)
-    default_restriction_operator(s1, s2; options...)
-end
-
-default_restriction_operator(s1::Dictionary, s2::Dictionary; options...) =
-    Restriction(s1, s2)
-
-
 # For convenience with dispatch, add the grids as extra arguments when only
 # GridBasis's are involved
 extension_operator(src::GridBasis, dest::GridBasis; options...) =
-    grid_extension_operator(src, dest, grid(src), grid(dest); options...)
-
-grid_extension_operator(src, dest, src_grid, dest_grid; options...) =
-    default_extension_operator(src, dest; options...)
+    grid_restriction_operator(dest, src, grid(dest), grid(src); options...)'
 
 # For convenience with dispatch, add the grids as extra arguments when only
 # GridBasis's are involved
@@ -132,18 +53,5 @@ restrict(s::Dictionary) = resize(s, restriction_size(s))
 restriction_operator(s1::Dictionary; options...) =
     restriction_operator(s1, restrict(s1); options...)
 
-adjoint(op::Extension)::DictionaryOperator = restriction_operator(dest(op), src(op))
-adjoint(op::Restriction)::DictionaryOperator = extension_operator(dest(op), src(op))
-
 # Transforming between dictionaries with the same type is the same as restricting
 hastransform(src::D, dest::D) where {D <: Dictionary} = true
-
-function transform_operator(src::D, dest::D) where {D <: Dictionary}
-    if length(src) > length(dest)
-        return Restriction(src, dest)
-    elseif length(src) < length(dest)
-        return Extension(src, dest)
-    else
-        return Identity(src, dest)
-    end
-end
