34f241c1f7a996dd412bd8b5c110f7e5e76ff8e8
Daan Huybrechs <daan.huybrechs@kuleuven.be>
2023-03-15 13:17:55 +0100

    FFTW plans

--- test/util/test_utilities.jl
+++ test/util/test_utilities.jl
@@ -4,18 +4,24 @@ using Test, FFTW, BasisFunctions, LinearAlgebra, SparseArrays
 # Verify types of FFT and DCT plans by FFTW
 # If anything changes here, the aliases in fouriertransforms.jl have to change as well
 d1 = FFTW.plan_fft!(zeros(Complex{Float64}, 10), 1:1)
-@test typeof(d1) == FFTW.cFFTWPlan{Complex{Float64},-1,true,1,UnitRange{Int64}}
+@test d1 isa FFTW.cFFTWPlan{Complex{Float64},-1,true,1,UnitRange{Int64}}
+@test d1 isa BasisFunctions.FFTPLAN
 d2 = FFTW.plan_fft!(zeros(Complex{Float64}, 10, 10), 1:2)
-@test typeof(d2) == FFTW.cFFTWPlan{Complex{Float64},-1,true,2,UnitRange{Int64}}
+@test d2 isa FFTW.cFFTWPlan{Complex{Float64},-1,true,2,UnitRange{Int64}}
+@test d2 isa BasisFunctions.FFTPLAN
 d3 = FFTW.plan_bfft!(zeros(Complex{Float64}, 10), 1:1)
-@test typeof(d3) == FFTW.cFFTWPlan{Complex{Float64},1,true,1,UnitRange{Int64}}
+@test d3 isa FFTW.cFFTWPlan{Complex{Float64},1,true,1,UnitRange{Int64}}
+@test d3 isa BasisFunctions.IFFTPLAN
 d4 = FFTW.plan_bfft!(zeros(Complex{Float64}, 10, 10), 1:2)
-@test typeof(d4) == FFTW.cFFTWPlan{Complex{Float64},1,true,2,UnitRange{Int64}}
+@test d4 isa FFTW.cFFTWPlan{Complex{Float64},1,true,2,UnitRange{Int64}}
+@test d4 isa BasisFunctions.IFFTPLAN
 
 d5 = FFTW.plan_dct!(zeros(10), 1:1)
-@test typeof(d5) == FFTW.DCTPlan{Float64,5,true}
+@test d5 isa FFTW.DCTPlan{Float64,5,true}
+@test d5 isa BasisFunctions.DCTII_PLAN
 d6 = FFTW.plan_idct!(zeros(10), 1:1)
-@test typeof(d6) == FFTW.DCTPlan{Float64,4,true}
+@test d6 isa FFTW.DCTPlan{Float64,4,true}
+@test d6 isa BasisFunctions.INV_DCTII_PLAN
 
 D = Diagonal(1:400)
 R = BasisFunctions.RestrictionIndexMatrix{Float64}((400,),2:3)
