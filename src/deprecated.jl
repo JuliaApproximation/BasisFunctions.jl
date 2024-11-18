c1cfd463b35c4b1f58665477d2813c64c721211a
Daan Huybrechs <daan.huybrechs@cs.kuleuven.be>
2024-11-18 13:11:19 +0100

    modify deprecations

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -2,24 +2,21 @@
 @deprecate roots(dict::Dictionary, coef::AbstractVector) expansion_roots(dict, coef)
 @deprecate roots(b::OPS) ops_roots(b)
 
-# No longer identify expansions with coefficient vector
-@deprecate iterate(e::Expansion) iterate(coefficients(e))
-@deprecate iterate(e::Expansion, state) iterate(coefficients(e), state)
-import Base: collect
-collect(e::Expansion) = error("you didn't!")
-# @deprecate collect(e::Expansion) coefficients(e)
-
-# Deprecate the below right away because it conflicts with other ways of computing
-# with expansions
-# import Base: BroadcastStyle
-# @deprecate BroadcastStyle(e::Expansion) Base.Broadcast.DefaultArrayStyle{dimension(e)}()
-
-getindex(e::Expansion, args...) = error("Oh no you did not")
-# @deprecate getindex(e::Expansion, args...) getindex(e.coefficients, args...)
-@deprecate setindex!(e::Expansion, args...) getindex(e.coefficients, args...)
-
 # support of a dictionary
 @deprecate support(dict::Dictionary, idx) dict_support(dict, idx)
 @deprecate support(part::Partition, idx) partition_support(part, idx)
 
 @deprecate moment(dict::Dictionary1d, idx; options...) dict_moment(dict, idx; options...)
+
+
+# No longer identify expansions with coefficient vector
+# This functionality was removed in v0.7. For documentation purposes we list deprecations here:
+
+# @deprecate iterate(e::Expansion) iterate(coefficients(e))
+# @deprecate iterate(e::Expansion, state) iterate(coefficients(e), state)
+# @deprecate collect(e::Expansion) coefficients(e)
+# @deprecate getindex(e::Expansion, args...) getindex(e.coefficients, args...)
+# @deprecate setindex!(e::Expansion, args...) getindex(e.coefficients, args...)
+
+# import Base: BroadcastStyle
+# @deprecate BroadcastStyle(e::Expansion) Base.Broadcast.DefaultArrayStyle{dimension(e)}()
