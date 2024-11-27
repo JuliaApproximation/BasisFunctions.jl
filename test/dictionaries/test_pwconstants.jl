40dfe4511908de5df842f50a6a0acf0d1fe2e0f7
Daan Huybrechs <daan.huybrechs@kuleuven.be>
2024-11-27 21:59:33 +0100

    fix test deprecation

--- test/dictionaries/test_pwconstants.jl
+++ test/dictionaries/test_pwconstants.jl
@@ -12,13 +12,13 @@ using BasisFunctions, GridArrays
     @test isorthogonal(H,discretemeasure(MidpointEquispacedGrid(10,0,1)))
 
     @test support(H) == Interval{:closed,:open}(0,1)
-    @test support(H,1) == Interval{:closed,:open}(0,0.1)
+    @test BasisFunctions.dict_support(H,1) == Interval{:closed,:open}(0,0.1)
 
     @test 0 ∈ support(H)
     @test 1 ∉ support(H)
 
-    @test 0 ∈ support(H,1)
-    @test .1 ∉ support(H,1)
+    @test 0 ∈ BasisFunctions.support(H,1)
+    @test .1 ∉ BasisFunctions.support(H,1)
 
     @test gram(H,discretemeasure(g)) ≈ IdentityOperator(H)
     @test evaluation(H,g)≈IdentityOperator(H)
